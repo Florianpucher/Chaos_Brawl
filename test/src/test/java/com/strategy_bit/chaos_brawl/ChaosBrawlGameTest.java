3c03ac3689844a9ce7e6535a7f341e3ed251ea42
Alexander Isopp <alisopp@edu.aau.at>
2018-03-28 18:20:21 +0200

    added basic test module structure
    Signed-off-by: Alexander Isopp <alisopp@edu.aau.at>

---
+++ test/src/test/java/com/strategy_bit/chaos_brawl/ChaosBrawlGameTest.java
@@ -0,0 +1,28 @@
+package com.strategy_bit.chaos_brawl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author AIsopp
+ * @version 1.0
+ * @since 28.03.2018
+ */
+public class ChaosBrawlGameTest {
+
+    private ChaosBrawlGame chaosBrawlGame;
+
+
+    @Before
+    public void initialize() {
+        chaosBrawlGame = new ChaosBrawlGame();
+
+    }
+
+
+    @Test
+    public void test() {
+
+    }
+}
