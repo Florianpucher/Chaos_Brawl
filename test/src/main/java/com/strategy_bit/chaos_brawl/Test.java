558dc90a5eec30cb6958bf006d7f472f9a9298f1
Alexander Isopp <alisopp@edu.aau.at>
2018-03-29 14:43:17 +0200

    changed module structure from core and added pit test support -moved core test to core module, because otherwise pit test will not work -removed test module from gradle project, files still exists!!!
    Signed-off-by: Alexander Isopp <alisopp@edu.aau.at>

---
+++ test/src/main/java/com/strategy_bit/chaos_brawl/Test.java
@@ -0,0 +1,12 @@
+package com.strategy_bit.chaos_brawl;
+
+/**
+ * @author AIsopp
+ * @version 1.0
+ * @since 28.03.2018
+ */
+public class Test {
+    public boolean test(int i){
+        return i == 5;
+    }
+}
