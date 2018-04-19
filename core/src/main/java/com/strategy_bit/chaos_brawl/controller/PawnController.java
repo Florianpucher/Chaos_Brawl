343f0d0f78037f8c79fc2308e2f77f42bf70bac2
rtosir@outlook.com <rtosir@outlook.com>
2018-04-19 22:13:18 +0200

    #49: refactoring

--- core/src/main/java/com/strategy_bit/chaos_brawl/controller/PawnController.java
+++ core/src/main/java/com/strategy_bit/chaos_brawl/controller/PawnController.java
@@ -1,8 +1,14 @@
 package com.strategy_bit.chaos_brawl.controller;
 
 
+import com.badlogic.ashley.core.Entity;
 import com.strategy_bit.chaos_brawl.ResourceSystem.Resource;
 import com.strategy_bit.chaos_brawl.ResourceSystem.ResourceGold;
+import com.strategy_bit.chaos_brawl.ashley.entity.Base;
+import com.strategy_bit.chaos_brawl.ashley.entity.Player;
+import com.strategy_bit.chaos_brawl.ashley.entity.PlayerClone;
+import com.strategy_bit.chaos_brawl.ashley.entity.Tower;
+import com.strategy_bit.chaos_brawl.types.UnitType;
 import com.strategy_bit.chaos_brawl.util.Boundary;
 import com.strategy_bit.chaos_brawl.world.InputHandler;
 import com.strategy_bit.chaos_brawl.world.World;
@@ -10,6 +16,7 @@ import com.strategy_bit.chaos_brawl.world.World;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+
 
 /**
  * Interface for communication between player(AI or human) and game
@@ -31,15 +38,21 @@ public abstract class PawnController {
     public PawnController(InputHandler inputHandler, Boundary spawnArea){
         this.inputHandler = inputHandler;
         this.spawnArea = spawnArea;
+        createResource();
     }
     public void tick(){
         for (Resource r :
                 resources) {
             r.add(rate);
+            float width=MAX_WIDTH*(float) r.percentageFull();
+            //TODO paint bars somewhere on screen with percentage
+
         }
+
+
     }
-    public void createResource(int teamId){
-        Resource resource=new ResourceGold(teamId);
+    public void createResource(){
+        Resource resource=new ResourceGold();
         resources.add(resource);
     }
 
@@ -51,5 +64,19 @@ public abstract class PawnController {
             }
         }
         return false;
+    }
+    public boolean spawnUnit(UnitType unitType){
+        double cost=Double.MIN_VALUE;
+        switch (unitType){
+            case RANGED:
+                cost=PlayerClone.COST;
+                break;
+            case MELEE:
+                cost=Player.COST;
+                break;
+            default:
+                break;
+        }
+        return checkAndSubtract(cost,"Gold");
     }
 }
