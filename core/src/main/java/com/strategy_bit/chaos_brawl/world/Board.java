b4bf41887d872e5e4fb357785aeeff93bb96bf1c
Alexander Isopp <alisopp@edu.aau.at>
2018-04-24 15:47:38 +0200

    added documentation
    Signed-off-by: Alexander Isopp <alisopp@edu.aau.at>

--- core/src/main/java/com/strategy_bit/chaos_brawl/world/Board.java
+++ core/src/main/java/com/strategy_bit/chaos_brawl/world/Board.java
@@ -9,6 +9,20 @@ import com.badlogic.gdx.math.Vector2;
 public interface Board {
     //public BoardType getBoardType();
     public Tile[][] getTileBoard();
+
+    /**
+     * returns a matrix like<br>
+     * current implementation 0 entries stays for not walkable and 1 for walkable<br>
+     * 0,0 0,1 0,2 0,3 0,4 0,5 0,WIDTH <br>
+     * 1,0 1,1 <br>
+     * 2,0 2,1 <br>
+     * 3,0 3,1 <br>
+     * 4,0 4,1 <br>
+     * 5,0 5,1 <br>
+     * 6,0 6,1 <br>
+     * HEIGHT,0 <br>
+     * @return a matrix in the form of int[HEIGHT][WIDTH]
+     */
     public int[][] boardToMatrix();
 
     /**
