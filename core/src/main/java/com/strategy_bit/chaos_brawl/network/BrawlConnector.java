517e29c91458468556e235a1926ba7547a18b65c
Alexander Isopp <alisopp@edu.aau.at>
2018-04-27 00:11:58 +0200

    added brawlConnector interface
    Signed-off-by: Alexander Isopp <alisopp@edu.aau.at>

---
+++ core/src/main/java/com/strategy_bit/chaos_brawl/network/BrawlConnector.java
@@ -0,0 +1,13 @@
+package com.strategy_bit.chaos_brawl.network;
+
+import com.strategy_bit.chaos_brawl.world.MultiplayerInputHandler;
+
+/**
+ * @author AIsopp
+ * @version 1.0
+ * @since 26.04.2018
+ */
+public interface BrawlConnector {
+    public MultiplayerInputHandler getInputHandler();
+    void setMultiplayerInputHandler(MultiplayerInputHandler multiplayerInputHandler);
+}
