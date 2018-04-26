951809d834511bba5efb2b5558706319711cd1fd
Alexander Isopp <alisopp@edu.aau.at>
2018-04-26 22:03:39 +0200

    added implementation for notify methods
    Signed-off-by: Alexander Isopp <alisopp@edu.aau.at>

--- core/src/main/java/com/strategy_bit/chaos_brawl/player_input_output/OtherPlayerController.java
+++ core/src/main/java/com/strategy_bit/chaos_brawl/player_input_output/OtherPlayerController.java
@@ -1,5 +1,9 @@
 package com.strategy_bit.chaos_brawl.player_input_output;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.strategy_bit.chaos_brawl.network.BrawlMultiplayer;
+import com.strategy_bit.chaos_brawl.types.UnitType;
 import com.strategy_bit.chaos_brawl.util.Boundary;
 import com.strategy_bit.chaos_brawl.world.InputHandler;
 
@@ -11,7 +15,35 @@ import com.strategy_bit.chaos_brawl.world.InputHandler;
  * @since 19.04.2018
  */
 public class OtherPlayerController extends PawnController {
-    public OtherPlayerController(int teamID, InputHandler inputHandler, Boundary spawnArea) {
+    private BrawlMultiplayer brawlMultiplayer;
+    public OtherPlayerController(int teamID, InputHandler inputHandler, Boundary spawnArea, BrawlMultiplayer brawlMultiplayer) {
         super(teamID, inputHandler, spawnArea);
+        this.brawlMultiplayer = brawlMultiplayer;
+    }
+
+
+    @Override
+    public void tick() {
+        brawlMultiplayer.sendTick();
+    }
+
+    @Override
+    public void notifyAboutAttacking(long unitID_Attacker, long unitID_Victim) {
+
+    }
+
+    @Override
+    public void notifyAboutDeletingUnit(long unitID) {
+        brawlMultiplayer.sendEntityDeleteMsg(unitID);
+    }
+
+    @Override
+    public void notifyAboutMoving(long unitID, Array<Vector2> movingPositions) {
+        brawlMultiplayer.sendEntityMovingMessage(unitID,movingPositions);
+    }
+
+    @Override
+    public void notifyAboutSpawning(Vector2 worldPosition, UnitType unitType, int teamID) {
+        brawlMultiplayer.sendEntitySpawnMsg(worldPosition, unitType,teamID);
     }
 }
