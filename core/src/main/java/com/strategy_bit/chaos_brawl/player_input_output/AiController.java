cf541ed9d5f10fe70dae0a46f97171b452a787fb
IIIlllllIlIllIllI <rtosir@outlook.com>
2018-06-15 11:15:14 +0200

    more units for AI

--- core/src/main/java/com/strategy_bit/chaos_brawl/player_input_output/AiController.java
+++ core/src/main/java/com/strategy_bit/chaos_brawl/player_input_output/AiController.java
@@ -4,9 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.strategy_bit.chaos_brawl.managers.AssetManager;
 import com.strategy_bit.chaos_brawl.util.SpawnArea;
 import com.strategy_bit.chaos_brawl.world.InputHandler;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
@@ -73,8 +76,15 @@ public class AiController extends PawnController implements Runnable{
                 float y =  MathUtils.random(spawnArea.getY(), spawnArea.getY() + spawnArea.getHeight());
                 final Vector2 spawnPosition = new Vector2(x, y);
                 Gdx.app.postRunnable(() -> {
-                    if (spawnUnit(0)) {
-                        inputHandler.createEntityWorldCoordinates(spawnPosition, 0, teamID);
+                    Set<Integer> set=new HashSet<>();
+                    for (int i = 0; i < 6; i++) {
+                        set.add(i);
+                    }
+                    set.add(18);
+                    set.add(19);
+                    int unitId=(Integer) set.toArray()[(int)(Math.random()*set.size())];
+                    if (spawnUnit(unitId)) {
+                        inputHandler.createEntityWorldCoordinates(spawnPosition, unitId, teamID);
                     }
                 });
 
