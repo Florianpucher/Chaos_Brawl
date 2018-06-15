249125e861287b62e46a8be765dc5361985a9c87
Alexander Isopp <alisopp@edu.aau.at>
2018-06-15 16:30:56 +0200

    bugfix ai controller does stop now when someone calls the method stop
    Signed-off-by: Alexander Isopp <alisopp@edu.aau.at>

--- core/src/main/java/com/strategy_bit/chaos_brawl/player_input_output/AiController.java
+++ core/src/main/java/com/strategy_bit/chaos_brawl/player_input_output/AiController.java
@@ -85,6 +85,10 @@ public class AiController extends PawnController implements Runnable{
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    if(!isRunning)
+                    {
+                        break;
+                    }
                 }
                 Gdx.app.postRunnable(() -> {
 
