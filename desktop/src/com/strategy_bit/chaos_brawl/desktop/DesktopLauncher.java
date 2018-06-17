9e8ac1a09c0da9d13d249a64e27a4668dd6c40f8
rtosir@outlook.com <rtosir@outlook.com>
2018-06-17 20:24:13 +0200

    removed on screen android ui (true fullscreen)

--- desktop/src/com/strategy_bit/chaos_brawl/desktop/DesktopLauncher.java
+++ desktop/src/com/strategy_bit/chaos_brawl/desktop/DesktopLauncher.java
@@ -7,8 +7,11 @@ import com.strategy_bit.chaos_brawl.ChaosBrawlGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1024;
-		config.height = 768;
+		config.width = 1280;
+		config.height = 720;
+		config.resizable=false;
+		config.vSyncEnabled=false;
+		config.foregroundFPS=144;
 		new LwjglApplication(new ChaosBrawlGame(), config);
 	}
 }
