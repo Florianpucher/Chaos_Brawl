9720d3dcd70b8b34f6ff5aab945b194d835435c4
Engin92 <eorhan@edu.aau.at>
2018-06-18 22:37:10 +0200

    added missing menu sounds

--- core/src/main/java/com/strategy_bit/chaos_brawl/screens/menu_screens/ClientConnectToScreen.java
+++ core/src/main/java/com/strategy_bit/chaos_brawl/screens/menu_screens/ClientConnectToScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.strategy_bit.chaos_brawl.managers.AssetManager;
+import com.strategy_bit.chaos_brawl.managers.SoundManager;
 import com.strategy_bit.chaos_brawl.network.client.BrawlClient;
 import com.strategy_bit.chaos_brawl.network.client.BrawlClientImpl;
 import com.strategy_bit.chaos_brawl.network.network_handlers.NetworkConnectionHandler;
@@ -60,6 +61,9 @@ public class ClientConnectToScreen extends MenuScreen implements NetworkDiscover
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
+
+                SoundManager.getInstance().playSound("click");
+
                 String name = event.getListenerActor().getName();
                 if (name.equals(REFRESH)) {
                     brawlClient.discoverServers();
